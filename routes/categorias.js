d8f0e996bfd5c4b9a64a4cb349f27b114b1780b9
Florencia Urrejola Ferrer <florurre@gmail.com>
2021-11-12 16:33:25 -0300

    haaaaaaaaaaaaa

--- routes/categorias.js
+++
@@ -1,7 +0,0 @@
-var express = require ('express');
-var router = express.Router();
-const categoriasControllers = require("../controllers/categoriasControllers")
-
-router.get('/', categoriasControllers.getAll);
-router.post('/', categoriasControllers.create);
-module.exports = router;
